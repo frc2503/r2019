cea972c8abc9b8a5c0bce87257013cb193dc94a2
Hunter Kvalevog <hunterkva@gmail.com>
2019-02-09 23:13:42 -0600

    SetBothCylinders command

---
+++ r2019/src/main/java/frc/robot/commands/SetBothCylinders.java
@@ -0,0 +1,21 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+import frc.robot.subsystems.LiftSubsystem.CylinderState;
+
+public class SetBothCylinders extends CommandGroup {
+  /**
+   * Add your docs here.
+   */
+  public SetBothCylinders(CylinderState state) {
+    addParallel(new SetFrontCylinder(state));
+    addSequential(new SetBackCylinder(state));
+  }
+}
