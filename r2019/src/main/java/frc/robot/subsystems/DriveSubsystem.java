2b1b30468a4204556b9f496c3ca42459d7f6692f
hkva <hkva@protonmail.com>
2019-02-10 19:16:24 -0600

    Remove unneeded variables

--- r2019/src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ r2019/src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -20,33 +20,26 @@ public class DriveSubsystem extends Subsystem {
   // Put methods for controlling this subsystem
   // here. Call these from Commands.
 
-  // Talon SRs are used to control our 4 drive motors
-  private Talon m_talonFrontLeft;
-  private Talon m_talonFrontRight;
-  private Talon m_talonBackLeft;
-  private Talon m_talonBackRight;
-
-  private SpeedControllerGroup m_leftTalons;
-  private SpeedControllerGroup m_rightTalons;
-
   private DifferentialDrive m_drive;
 
   public DriveSubsystem() {
     // This constructor is called on robotInit
-    m_talonFrontLeft = new Talon(RobotMap.TALON_FRONT_LEFT);
-    m_talonFrontRight = new Talon(RobotMap.TALON_FRONT_RIGHT);
-    m_talonBackLeft = new Talon(RobotMap.TALON_BACK_LEFT);
-    m_talonBackRight = new Talon(RobotMap.TALON_BACK_RIGHT);
 
-    // Group Talons
-    m_leftTalons = new SpeedControllerGroup(m_talonFrontLeft, m_talonBackLeft);
-    m_rightTalons = new SpeedControllerGroup(m_talonFrontRight, m_talonBackRight);
+    // 4 Talon SRs are used to control our drive motors
+    SpeedControllerGroup leftTalons = new SpeedControllerGroup(
+      new Talon(RobotMap.TALON_FRONT_LEFT),
+      new Talon(RobotMap.TALON_BACK_LEFT)
+    );
+    SpeedControllerGroup rightTalons = new SpeedControllerGroup(
+      new Talon(RobotMap.TALON_FRONT_RIGHT),
+      new Talon(RobotMap.TALON_BACK_RIGHT)
+    );
 
     // Invert left side
-    m_leftTalons.setInverted(true);
+    leftTalons.setInverted(true);
 
     // Differential drive
-    m_drive = new DifferentialDrive(m_leftTalons, m_rightTalons);
+    m_drive = new DifferentialDrive(leftTalons, rightTalons);
   }
 
   @Override
