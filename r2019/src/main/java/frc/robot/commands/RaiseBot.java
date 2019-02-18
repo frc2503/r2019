f8300d4e6af0457507dffa47cf7ce8a6a1e0713d
hkva <hkva@protonmail.com>
2019-02-18 01:16:40 -0600

    Rename RaiseSolenoids to RaiseBot

--- r2019/src/main/java/frc/robot/commands/RaiseBot.java
+++ r2019/src/main/java/frc/robot/commands/RaiseBot.java
@@ -10,28 +10,12 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj.command.CommandGroup;
 import frc.robot.subsystems.CylinderState;
 
-public class RaiseSolenoids extends CommandGroup {
+public class RaiseBot extends CommandGroup {
   /**
    * Add your docs here.
    */
-  public RaiseSolenoids() {
-    // Add Commands here:
-    // e.g. addSequential(new Command1());
-    // addSequential(new Command2());
-    // these will run in order.
-
-    // To run multiple commands at the same time,
-    // use addParallel()
-    // e.g. addParallel(new Command1());
-    // addSequential(new Command2());
-    // Command1 and Command2 will run in parallel.
-
-    // A command group will require all of the subsystems that each member
-    // would require.
-    // e.g. if Command1 requires chassis, and Command2 requires arm,
-    // a CommandGroup containing them would require both the chassis and the
-    // arm.
-
+  public RaiseBot() {
+    
     addParallel(new SetFrontCylinder(CylinderState.kExtended));
     addParallel(new SetBackCylinder(CylinderState.kExtended));
   }
